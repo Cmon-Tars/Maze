d3a51a1f7fcd2633bf538d5a6098bd636baa3fb0
Cmon-Tars <jpadilla@techsoup.org>
2024-07-29 12:22:10 -0700

    readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Maze Solver
+
+This module provides a gym to test different search algorithims.  
+
+## How to run
+
+Fork this repo and run `python main.py` for default settings.  
+
+
+## Extend
+
+Edit the solve emthod to impelement new search algorithims
+
+
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def main():
     window_width = 900
     window_height = 900
     win = Window(window_width, window_height)
-    num_cols = 30
-    num_rows =30
-    cell_size_x =  15
-    cell_size_y =  15
+    num_cols = 40
+    num_rows = 40
+    cell_size_x =  20
+    cell_size_y =  20
     maze = Maze(x1 = round((window_width - num_cols*cell_size_x)/2), y1 = round((window_height - num_rows*cell_size_y)/2), num_cols = num_cols, num_rows = num_rows, cell_size_x =cell_size_x, cell_size_y =cell_size_y, win = win, seed=0)
     win.waitForClosed()
 
